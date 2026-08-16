1510016e96ef27efd3694cfee12598d4617016b9
Noorsaba <noorsabasafi007@gmail.com>
2026-08-17 00:00:33 +0200

    array methods

---
+++ day080.js
@@ -0,0 +1,5 @@
+const myarray = [1, 2, 3, 4, 5];
+
+const double = myarray.map((n) => n * 2);
+
+console.log(double);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,7 @@
-let count = 0;
+// array methods
+//# map
+
+const myarray = [1, 2, 3, 4, 5];
+const double = map.myarray((n) => n * 2);
+
+console.log(double);
