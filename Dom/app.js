2188ce96acd552cf1a2d8b37f1d4a88006974e5c
Noorsaba786 <noorsabasafi007@gmail.com>
2026-08-23 20:14:56 +0200

    mini Excercie with js selectors

--- Dom/app.js
+++ Dom/app.js
@@ -1,4 +1,4 @@
-const title = document.querySelector("#title");
+/* const title = document.querySelector("#title");
 const paragraphs = document.querySelectorAll(".text");
 
 const button = document.querySelector("#btn");
@@ -16,3 +16,32 @@ newTitle.innerText = "i am h1 ";
 newTitle.style.color = "pink";
 
 document.body.appendChild(newTitle);
+ */
+
+/* <!-- 
+//Selects the input, button, and list
+When the button is clicked, reads the input value
+Creates a new <li> with that text
+Appends it to the <ul>
+Clears the input after adding --> */
+
+const input = document.querySelector("#tasks");
+const button = document.querySelector("#btn");
+const list = document.querySelector("#parent");
+const heading = document.querySelector("h1");
+const container = document.querySelector("#wrapper");
+
+heading.addEventListener("click", () => {
+  container.classList.toggle("toggle");
+});
+
+button.addEventListener("click", () => {
+  let myElement = input.value;
+
+  const elemets = document.createElement("li");
+  elemets.textContent = myElement;
+
+  list.appendChild(elemets);
+
+  input.value = "";
+});
