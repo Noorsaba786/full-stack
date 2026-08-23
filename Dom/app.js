a39a0b13f7b582d9c5d1c0c796d544231a573fe6
Noorsaba786 <noorsabasafi007@gmail.com>
2026-08-23 23:41:50 +0200

    mini excercice improvement with css and toggle

--- Dom/app.js
+++ Dom/app.js
@@ -1,30 +1,3 @@
-/* const title = document.querySelector("#title");
-const paragraphs = document.querySelectorAll(".text");
-
-const button = document.querySelector("#btn");
-
-console.log(title);
-console.log(paragraphs);
-console.log(button);
-
-const addNewElement = document.createElement("p");
-addNewElement.innerHTML = "<strong>hey live is beautiful<strong>";
-document.body.appendChild(addNewElement);
-
-const newTitle = document.createElement("h1");
-newTitle.innerText = "i am h1 ";
-newTitle.style.color = "pink";
-
-document.body.appendChild(newTitle);
- */
-
-/* <!-- 
-//Selects the input, button, and list
-When the button is clicked, reads the input value
-Creates a new <li> with that text
-Appends it to the <ul>
-Clears the input after adding --> */
-
 const input = document.querySelector("#tasks");
 const button = document.querySelector("#btn");
 const list = document.querySelector("#parent");
@@ -36,9 +9,12 @@ heading.addEventListener("click", () => {
 });
 
 button.addEventListener("click", () => {
-  let myElement = input.value;
+  let myElement = input.value.trim();
+
+  if (myElement === "") return;
 
   const elemets = document.createElement("li");
+
   elemets.textContent = myElement;
 
   list.appendChild(elemets);
