1b2d76f9afc71c5461244c997de7262b5193c61f
Noorsaba786 <noorsabasafi007@gmail.com>
2026-08-29 12:16:22 +0200

    events exercices

--- Dom/Events/events.js
+++ Dom/Events/events.js
@@ -8,6 +8,17 @@ const addTask = () => {
 
   const li = document.createElement("li");
   li.textContent = task;
+
+  li.addEventListener("click", () => {
+    console.log("task clicked");
+    li.style.textDecoration = "line-through";
+    li.addEventListener("dblclick", () => {
+      console.log("task double clicked");
+      li.style.textDecoration = "line-through";
+      li.style.opacity = "0.5";
+    });
+  });
+
   list.appendChild(li);
   input.value = "";
 };
