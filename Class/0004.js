6522dfabf898eac909d4559b53fe6b646eeabf4f
Noorsaba786 <noorsabasafi007@gmail.com>
2026-09-01 22:08:45 +0200

    class exercices

---
+++ Class/0004.js
@@ -0,0 +1,16 @@
+// class example
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    return "hello my name is " + this.name + " and i have " + this.age;
+  }
+}
+
+const user1 = new User("saba", 16);
+
+console.log(user1.greet());
